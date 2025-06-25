5a498bc2d862491560b9f34eb6b94b70d6c8af15
aravhraj <aravhraj@gmail.com>
2025-06-25 20:34:26 +0530

    creating gui

---
+++ Frontend/GUI.py
@@ -0,0 +1,2 @@
+from PyQt5.QtWidgets import WApplication, QMainwindows, QTextEdit, QStackWindget, QWidget, QLineEdit, QGridlayout, QVBoxLayout, QHBoxLayout,QPushButton, QFrame, QLabel, QSizePolicy
+from PyQt5.QtGui
